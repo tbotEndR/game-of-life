4846eb95ce4582ecb05c3b5026d6d3dc1594cb24
tbot <thibaut.endress27@proton.me>
2025-03-07 10:23:45 +0100

    clean up unused example code

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,6 +1,6 @@
 #include "raylib.h"
 
-#define MAX_BUILDINGS   100
+#define CELL_SIZE 20
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -9,114 +9,81 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera split screen");
 
-    Rectangle player = { 400, 280, 40, 40 };
-    Rectangle buildings[MAX_BUILDINGS] = { 0 };
-    Color buildColors[MAX_BUILDINGS] = { 0 };
+    Rectangle player1 = { 200, 200, CELL_SIZE, CELL_SIZE };
 
-    int spacing = 0;
+    Camera2D camera1 = { 0 };
+    camera1.target = (Vector2){ 200, 200 };
+    camera1.offset = (Vector2){ 200.0f, 200.0f };
+    camera1.rotation = 0.0f;
+    camera1.zoom = 1.0f;
 
-    for (int i = 0; i < MAX_BUILDINGS; i++)
-    {
-        buildings[i].width = (float)GetRandomValue(50, 200);
-        buildings[i].height = (float)GetRandomValue(100, 800);
-        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
-        buildings[i].x = -6000.0f + spacing;
+    RenderTexture gameboard = LoadRenderTexture(screenWidth, screenHeight);
 
-        spacing += (int)buildings[i].width;
+    // Build a flipped rectangle the size of the split view to use for drawing later
+    Rectangle splitScreenRect = { 0.0f, 0.0f, (float)gameboard.texture.width, (float)-gameboard.texture.height };
 
-        buildColors[i] = (Color){ GetRandomValue(200, 240), GetRandomValue(200, 240), GetRandomValue(200, 250), 255 };
-    }
+    //  Draw grid into a texture once (since it never changes)
+    BeginTextureMode(gameboard);
+    ClearBackground(RAYWHITE);
+    BeginMode2D(camera1);
+    
+        for (int i = 0; i < screenWidth/CELL_SIZE + 1; i++)
+        {
+            DrawLineV((Vector2){(float)CELL_SIZE*i, 0}, (Vector2){ (float)CELL_SIZE*i, (float)screenHeight}, LIGHTGRAY);
+        }
 
-    Camera2D camera = { 0 };
-    camera.target = (Vector2){ player.x + 20.0f, player.y + 20.0f };
-    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
+        for (int i = 0; i < screenHeight/CELL_SIZE + 1; i++)
+        {
+            DrawLineV((Vector2){0, (float)CELL_SIZE*i}, (Vector2){ (float)screenWidth, (float)CELL_SIZE*i}, LIGHTGRAY);
+        }
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+        /*
+        for (int i = 0; i < screenWidth/CELL_SIZE; i++)
+        {
+            for (int j = 0; j < screenHeight/CELL_SIZE; j++)
+            {
+                DrawText(TextFormat("[%i,%i]", i, j), 10 + CELL_SIZE*i, 15 + CELL_SIZE*j, 5, LIGHTGRAY);
+            }
+        }
+        */
+
+        EndMode2D();
+    EndTextureMode();
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // Player movement
-        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
-        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
-
-        // Camera target follows player
-        camera.target = (Vector2){ player.x + 20, player.y + 20 };
-
-        // Camera rotation controls
-        if (IsKeyDown(KEY_A)) camera.rotation--;
-        else if (IsKeyDown(KEY_S)) camera.rotation++;
-
-        // Limit camera rotation to 80 degrees (-40 to 40)
-        if (camera.rotation > 40) camera.rotation = 40;
-        else if (camera.rotation < -40) camera.rotation = -40;
-
-        // Camera zoom controls
-        camera.zoom += ((float)GetMouseWheelMove()*0.05f);
-
-        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
-        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
-
-        // Camera reset (zoom and rotation)
-        if (IsKeyPressed(KEY_R))
-        {
-            camera.zoom = 1.0f;
-            camera.rotation = 0.0f;
-        }
+        
+        BeginTextureMode(gameboard);
+        BeginMode2D(camera1);
+            //  draw active cells into texture
+        EndMode2D();
+        EndTextureMode();
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            BeginMode2D(camera);
-
-                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
-
-                for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
-
-                DrawRectangleRec(player, RED);
-
-                DrawLine((int)camera.target.x, -screenHeight*10, (int)camera.target.x, screenHeight*10, GREEN);
-                DrawLine(-screenWidth*10, (int)camera.target.y, screenWidth*10, (int)camera.target.y, GREEN);
-
-            EndMode2D();
-
-            DrawText("SCREEN AREA", 640, 10, 20, RED);
-
-            DrawRectangle(0, 0, screenWidth, 5, RED);
-            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
-
-            DrawRectangle( 10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 250, 113, BLUE);
-
-            DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
-            DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
-            DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
-            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
-
+            ClearBackground(BLACK);
+            DrawTextureRec(gameboard.texture, splitScreenRect, (Vector2){ 0, 0 }, WHITE);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    UnloadRenderTexture(gameboard);    // Unload render texture
+
+    CloseWindow();                      // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
