83c88eac98729d7eee004c3a3948112de2e7a41b
tbot <thibaut.endress27@proton.me>
2025-03-07 21:15:23 +0100

    dunno forgot what I did

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "raymath.h"
+#include <stdio.h>
 
 #define CELL_SIZE 20
 
@@ -12,17 +14,17 @@ int main(void)
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera split screen");
+    InitWindow(screenWidth, screenHeight, "game of life");
 
-    Rectangle player1 = { 200, 200, CELL_SIZE, CELL_SIZE };
-
-    Camera2D camera1 = { 0 };
-    camera1.target = (Vector2){ 200, 200 };
-    camera1.offset = (Vector2){ 200.0f, 200.0f };
-    camera1.rotation = 0.0f;
-    camera1.zoom = 1.0f;
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ 200, 200 };
+    camera.offset = (Vector2){ 200.0f, 200.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
     RenderTexture gameboard = LoadRenderTexture(screenWidth, screenHeight);
+
+    char cursorTextBuffer[10];
 
     // Build a flipped rectangle the size of the split view to use for drawing later
     Rectangle splitScreenRect = { 0.0f, 0.0f, (float)gameboard.texture.width, (float)-gameboard.texture.height };
@@ -30,7 +32,7 @@ int main(void)
     //  Draw grid into a texture once (since it never changes)
     BeginTextureMode(gameboard);
     ClearBackground(RAYWHITE);
-    BeginMode2D(camera1);
+    BeginMode2D(camera);
     
         for (int i = 0; i < screenWidth/CELL_SIZE + 1; i++)
         {
@@ -40,20 +42,11 @@ int main(void)
         for (int i = 0; i < screenHeight/CELL_SIZE + 1; i++)
         {
             DrawLineV((Vector2){0, (float)CELL_SIZE*i}, (Vector2){ (float)screenWidth, (float)CELL_SIZE*i}, LIGHTGRAY);
-        }
-
-        /*
-        for (int i = 0; i < screenWidth/CELL_SIZE; i++)
-        {
-            for (int j = 0; j < screenHeight/CELL_SIZE; j++)
-            {
-                DrawText(TextFormat("[%i,%i]", i, j), 10 + CELL_SIZE*i, 15 + CELL_SIZE*j, 5, LIGHTGRAY);
-            }
-        }
-        */
+        }        
 
         EndMode2D();
     EndTextureMode();
+    //--------------------------------------------------------------------------------------
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -63,10 +56,10 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        
+       //   if there's a click, check location and parse the active cell
         BeginTextureMode(gameboard);
-        BeginMode2D(camera1);
-            //  draw active cells into texture
+        BeginMode2D(camera);
+        //  draw active cells into texture
         EndMode2D();
         EndTextureMode();
         //----------------------------------------------------------------------------------
@@ -76,13 +69,13 @@ int main(void)
         BeginDrawing();
             ClearBackground(BLACK);
             DrawTextureRec(gameboard.texture, splitScreenRect, (Vector2){ 0, 0 }, WHITE);
+            DrawTextEx(GetFontDefault(), TextFormat("[%i|%i]", GetMouseX(), GetMouseY()), Vector2Add(GetMousePosition(), (Vector2){ -44, -24 }), 20, 2, BLACK);
         EndDrawing();
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadRenderTexture(gameboard);    // Unload render texture
-
+    UnloadRenderTexture(gameboard);     // Unload render texture
     CloseWindow();                      // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
--- game/src/user_input.h
+++
@@ -1,6 +0,0 @@
-#ifndef USER_INPUT_H
-#define USER_INPUT_H
-
-
-
-#endif
